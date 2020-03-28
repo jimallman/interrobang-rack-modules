85f06311924a4868e3c13a93774e08623f290d0d
Jim Allman <jim@ibang.com>
2020-03-28 15:13:40 -0400

    Simple working version, not yet editable.

--- src/ScribbleStrip.cpp
+++ src/ScribbleStrip.cpp
@@ -1,53 +1,65 @@
 #include "plugin.hpp"
 
-
 struct ScribbleStrip : Module {
-	enum ParamIds {
-		PITCH_PARAM,
-		NUM_PARAMS
-	};
-	enum InputIds {
-		PITCH_INPUT,
-		NUM_INPUTS
-	};
-	enum OutputIds {
-		SINE_OUTPUT,
-		NUM_OUTPUTS
-	};
-	enum LightIds {
-		BLINK_LIGHT,
-		NUM_LIGHTS
-	};
+    std::string scribbleText = "Rt-click to edit";
 
-	ScribbleStrip() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+    json_t *dataToJson() override {
+        json_t *rootJ = json_object();
+        json_object_set_new(rootJ, "labelText", json_string(scribbleText.c_str()));
+        return rootJ;
+    }
+    void dataFromJson(json_t *rootJ) override {
+        json_t *labelTextJ = json_object_get(rootJ, "labelText");
+        if (labelTextJ) {
+            scribbleText = json_string_value(labelTextJ);
+        }
+    }
+
+    void process(const ProcessArgs& args) override {
+    }
+};
+
+
+struct ScribbleWidget : TransparentWidget {
+	ScribbleStrip *module;
+    std::shared_ptr<Font> font;
+
+	ScribbleWidget() {
+		font = APP->window->loadFont(asset::plugin(pluginInstance, "res/mad-midnight-marker-font/MadMidnightMarker-na91.ttf"));
+        // N.B. nanovg seems to only support .ttf fonts, *not* .otf!
 	}
 
-	void process(const ProcessArgs& args) override {
+	void draw(const DrawArgs &args) override {
+        // show an example message in the module browser
+        std::string scrText = module ? module->scribbleText : "Your message here!";
+        // truncate displayed text?
+		std::string to_display = "";
+		for (int i=0; i<20; i++) to_display = to_display + scrText[i];
+		nvgFontSize(args.vg, 20);
+		nvgFontFaceId(args.vg, font->handle);
+		nvgTextLetterSpacing(args.vg, 0);
+		nvgFillColor(args.vg, nvgRGBA(0xff, 0xff, 0xff, 0xff));
+		nvgRotate(args.vg, -M_PI / 2.0f);
+		nvgTextBox(args.vg, 8, 8, 350, to_display.c_str(), NULL);
 	}
 };
 
 
+
 struct ScribbleStripWidget : ModuleWidget {
-	ScribbleStripWidget(ScribbleStrip* module) {
-		setModule(module);
-		setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/ScribbleStrip.svg")));
+    ScribbleStripWidget(ScribbleStrip* module) {
+        setModule(module);
+        setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/ScribbleStrip.svg")));
 
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-
-		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, ScribbleStrip::PITCH_PARAM));
-
-		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, ScribbleStrip::PITCH_INPUT));
-
-		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, ScribbleStrip::SINE_OUTPUT));
-
-		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, ScribbleStrip::BLINK_LIGHT));
-	}
+        ScribbleWidget *msgDisplay = new ScribbleWidget();
+		msgDisplay->box.pos = Vec(18, 333);
+		msgDisplay->box.size = Vec(130, 250);
+		msgDisplay->module = module;
+		addChild(msgDisplay);
+    }
 };
 
 
 Model* modelScribbleStrip = createModel<ScribbleStrip, ScribbleStripWidget>("ScribbleStrip");
+
+/* vim: set autoindent noexpandtab tabstop=4 shiftwidth=4: */
