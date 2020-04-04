1ddff57de1179f24ef598abfdfc7a8717bbe548d
Jim Allman <jim@ibang.com>
2020-04-03 23:55:33 -0400

    More work on "Whisper" module.
    This is a combined inverter+delay, but it's not working out so far.

--- src/Whisper.cpp
+++ src/Whisper.cpp
@@ -1,5 +1,7 @@
 #include "plugin.hpp"
+#include "samplerate.h"
 
+#define HISTORY_SIZE (1<<21)
 
 struct Whisper : Module {
 	enum ParamIds {
@@ -15,17 +17,83 @@ struct Whisper : Module {
 		OUTPUTSIGNAL_OUTPUT,
 		NUM_OUTPUTS
 	};
-	enum LightIds {
-		NUM_LIGHTS
-	};
+
+    // set up sound buffers (adapted from VCV Delay modue)
+    dsp::DoubleRingBuffer<float, HISTORY_SIZE> historyBuffer;
+	dsp::DoubleRingBuffer<float, 16> outBuffer;
+	SRC_STATE* src;
+    //float delay = 0.f;     // 0.0 is a real-time mix; 10.0 delays the wet signal by 1 second
+    //float wetDryMix = 0.f; // 0.0 is completely dry, 10.0 is completely wet
 
 	Whisper() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(DELAY_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(WETDRYMIX_PARAM, 0.f, 1.f, 0.f, "");
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS);
+		configParam(DELAY_PARAM, 0.f, 1.f, 0.6f, "Time", " s", 1.f / 1e-3, 1e-3);
+		configParam(WETDRYMIX_PARAM, 0.f, 1.f, 0.5f, "Mix", "%", 0, 100);
+
+		src = src_new(SRC_SINC_FASTEST, 1, NULL);
+		assert(src);
 	}
 
-	void process(const ProcessArgs& args) override {
+    void process(const ProcessArgs &args) override {
+        /* Audio signals are typically +/-5V
+         * https://vcvrack.com/manual/VoltageStandards.html
+         * ...but theoretically the input could be CV, etc.
+         */
+        float in = inputs[INPUTSIGNAL_INPUT].getVoltage();
+        float dry = in;
+        float wet = 0.f;
+
+        // Compute delay time in seconds
+		float delay = params[DELAY_PARAM].getValue();
+		delay = clamp(delay, 0.f, 1.f);
+		delay = 1e-3 * std::pow(10.f / 1e-3, delay); // convert to sec?
+		// Number of delay samples
+		float index = std::round(delay * args.sampleRate);
+
+        // invert the signal, store/buffer the result
+        float inverse = -(in);
+		if (!historyBuffer.full()) {
+			historyBuffer.push(inverse);
+		}
+
+
+        // How many samples do we need consume to catch up?
+		float consume = index - historyBuffer.size();
+        if (outBuffer.empty()) {
+			double ratio = 1.f;
+			if (std::fabs(consume) >= 16.f) {
+				// Here's where the delay magic is. Smooth the ratio depending on how divergent we are from the correct delay time.
+				ratio = std::pow(10.f, clamp(consume / 10000.f, -1.f, 1.f));
+                //DEBUG("WHISPER - process 2a2, smoothed ratio is %f", ratio);
+			}
+			SRC_DATA srcData;
+			srcData.data_in = (const float*) historyBuffer.startData();
+			srcData.data_out = (float*) outBuffer.endData();
+			srcData.input_frames = std::min((int) historyBuffer.size(), 16);
+			srcData.output_frames = outBuffer.capacity();
+			srcData.end_of_input = false;
+			srcData.src_ratio = ratio;
+			src_process(src, &srcData);
+			historyBuffer.startIncr(srcData.input_frames_used);
+
+			outBuffer.endIncr(srcData.output_frames_gen);  /* BANG, here we crash */
+
+		}
+
+        // reach into history buffer for delayed sample
+        if (!outBuffer.empty()) {
+			wet = outBuffer.shift();
+		}
+
+        // mix using wet/dry value (from 0 to 1.0)
+        float mix = params[WETDRYMIX_PARAM].getValue();
+        mix = clamp(mix, 0.f, 1.f);
+        //DEBUG("mix ratio is %f", mix);
+        float out = crossfade(dry, wet, mix);
+        out = clamp(out, -5.f, 5.f);
+
+        // Compute the sine output
+        outputs[OUTPUTSIGNAL_OUTPUT].setVoltage(out);
 	}
 };
 
@@ -49,5 +117,6 @@ struct WhisperWidget : ModuleWidget {
 	}
 };
 
-
 Model* modelWhisper = createModel<Whisper, WhisperWidget>("Whisper");
+
+/* vim: set autoindent noexpandtab tabstop=4 shiftwidth=4: */
